d78fc18f1790acec6f6d848b443c29deda21165f
SIMONE <simonecipu@gmail.com>
2022-09-29 15:11:39 +0200

    commit

--- src/main/java/it/mongo/repository/dto/CustomerDto.java
+++ src/main/java/it/mongo/repository/dto/CustomerDto.java
@@ -5,41 +5,18 @@
  */
 package it.mongo.repository.dto;
 
+import lombok.Data;
+
 /**
  * @author simonecipullo
  */
+@Data
 public class CustomerDto {
 
     private String name;
 
-
     private String surname;
 
-
     private String company;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
 
 }
--- src/main/java/it/mongo/repository/dto/ProductDto.java
+++ src/main/java/it/mongo/repository/dto/ProductDto.java
@@ -5,15 +5,15 @@
  */
 package it.mongo.repository.dto;
 
+import lombok.Data;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  *
  * @author simonecipullo
  */
+@Data
 public class ProductDto {
 
     private String serialNumber;
@@ -23,38 +23,4 @@ public class ProductDto {
     private String name;
     
      private List<CustomerDto> customer=new ArrayList<>();
-
-    public List<CustomerDto> getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(List<CustomerDto> customer) {
-        this.customer = customer;
-    }
-
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
